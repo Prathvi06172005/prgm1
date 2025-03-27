75bbf1d83fa06de7f1ce028e96f019a467e1559d
Prathvi06172005 <prathvi.23cs103@sode-edu.in>
2025-03-27 22:23:59 +0530

    Add files via upload

--- Prg24.java
+++ Prg24.java
@@ -1,15 +1,14 @@
-
-package javaclass;
-public class Prg24 {
-    public static void main(String[] args) {
-        char ch='a';
-        for(int i=0;i<5;i++){
-            for(int j=0;j<5;j++){
-                System.out.print(ch+" ");
-                ch++;
-            }
-            ch='a';
-            System.out.println();
-        }
-    }
+public class Prg24{
+public static void main (String[]args){
+int n=5;
+for(int i=0;i<n;i++){
+for(int j=0;j<n;j++){
+if(i==j||i+j==n-1)
+{
+System.out.print("*"+" ");
 }
+else{
+System.out.print(" "+" ");
+}}
+System.out.println();
+}}}
