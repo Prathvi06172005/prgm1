f5f83fca212456df7979d7aba7f46d8c1d9c7773
Prathvi06172005 <prathvi.23cs103@sode-edu.in>
2025-03-28 10:42:23 +0530

    Add files via upload

--- Prg34.java
+++ Prg34.java
@@ -1,21 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaclass;
-
-/**
- *
- * @author SMV
- */
+import java.util.Scanner;
 public class Prg34 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scn=new Scanner(System.in);
+        int a=scn.nextInt();
+        int b=scn.nextInt();
+        double c=scn.nextDouble();
+        System.out.println(a +" "+b+" "+c);
+               
+        
+        
     }
     
 }
